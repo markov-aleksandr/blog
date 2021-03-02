9b34226175a34b1926305108706fd66324ee1ebf
derzhavocln <68552916+derzhavocln@users.noreply.github.com>
2021-03-02 22:36:15 +0200

    test

--- public/index.php
+++ public/index.php
@@ -1,7 +1,2 @@
 <?php
-session_start();
-//var_dump($_SESSION['id']);
-$_SESSION['id'] = 1;
-ini_set('display_errors', 1);
-require "../vendor/autoload.php";
-require "../routes/routes.php";
+phpinfo();
